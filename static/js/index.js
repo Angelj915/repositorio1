6d6d544cf007bcd8ff423e2a13fcff588a1b95ce
Angelj915 <81207912+Angelj915@users.noreply.github.com>
2021-04-07 16:01:08 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@ function estado() {
 	console.log("estado");
 	//document.getElementById("sensor").innerHTML="CLICK";
 	message = new Paho.MQTT.Message("estado");
-    	message.destinationName = "ajmorocho.fie@unach.edu.ec/tema2";
+    	message.destinationName = "ajmorocho.fie@unach.edu.ec/tema1";
     	client.send(message);
 	
 	
