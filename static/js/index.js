560d8d09161aa660bffcb779c17717a8d632e10b
Angelj915 <81207912+Angelj915@users.noreply.github.com>
2021-04-21 22:42:02 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -64,10 +64,10 @@ function historial() {
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
 	  msm=message.payloadString;
-	  if(msm[0]=="1"){
+	  if(msm[22]=="a"){
 		document.getElementById("sensor3").innerHTML=msm;  
 	  }
-	  if(msm[0]=="2"){
+	  if(msm[22]=="b"){
 		document.getElementById("sensor4").innerHTML=msm;  
 	  }
 	  if(msm=="SEN1-ON"){
